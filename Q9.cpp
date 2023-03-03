1e8976aad6392cec0417465d8d0965a14b0bad71
xyval <111503969+xyval@users.noreply.github.com>
2023-03-03 14:57:57 +0800

    Create Q9.cpp

---
+++ Q9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n[10];
+	int x;
+	srand(time(0));
+	for (int i = 0; i < 10; i++) {
+		n[i] = rand() % 10 + 1;
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << n[i] << " ";
+	}
+	cout << "\nvalue to find: ";
+	cin >> x; 
+	bool exist = 0;
+	for (int i = 0; i < 10; i++) {
+		if (n[i] == x) {
+			cout << x << " is in slot " << i << ".\n";
+			exist = 1; 
+		}
+	}
+	if (!exist)
+		cout << x << " is not in the array. \n";
+}
